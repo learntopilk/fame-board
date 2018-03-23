6e88a43eacdd1997c5e4111e756fcd3243805690
learntopilk <jonsterivilja@gmail.com>
2018-03-23 19:43:08 +0200

    Structural changes

---
+++ src/main/java/Webserver/Tykkays.java
@@ -0,0 +1,7 @@
+
+package Webserver;
+
+
+public class Tykkays {
+    
+}
---
+++ src/main/java/Webserver/Viesti.java
@@ -0,0 +1,17 @@
+package Webserver;
+
+
+public class Viesti {
+    
+    private String otsikko;
+    private String viesti;
+    
+    public Viesti(String otsikko, String sisalto) {
+        
+        this.viesti = sisalto;
+        this.otsikko = otsikko;
+    }
+
+
+    
+}
