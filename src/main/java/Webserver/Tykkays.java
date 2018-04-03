bcdc8340f12faf2f035485d7ad28608e6df76fda
learntopilk <jonsterivilja@gmail.com>
2018-04-03 22:01:08 +0300

    Begun implementing likes
    The like object has been created, also added a "number of likes" variable to viesti.java so as to optimize fetching the number of likes a post has. Database table adjustment not done on this yet.

--- src/main/java/Webserver/Tykkays.java
+++ src/main/java/Webserver/Tykkays.java
@@ -4,4 +4,25 @@ package Webserver;
 
 public class Tykkays {
     
+    public int viesti_id;
+    public int kayttaja_id;
+    
+    public Tykkays(){
+        
+    }
+    
+    public Tykkays(int viestiId, int kayttajaId) {
+        this.viesti_id = viestiId;
+        this.kayttaja_id = kayttaja_id;
+        
+    }
+    
+    public int getViestiId() {
+        return this.viesti_id;
+    }
+    
+    public int getKayttajaId() {
+        return this.kayttaja_id;
+    }
+    
 }
